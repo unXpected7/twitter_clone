3fc466ec058dd5051ca61edf4d6d74f6f8856cd5
hikaaam <andrenuryana@gmail.com>
2021-12-11 16:24:58 +0700

    add hardcode account database

---
+++ App/Database/index.tsx
@@ -0,0 +1,9 @@
+const Database = {
+  Account: {
+    name: "Faiz",
+    image:
+      "https://th.bing.com/th/id/OIP.rRHSWX6aTkYTYWWg5BZwAAHaEK?w=286&h=180&c=7&r=0&o=5&pid=1.7",
+  },
+};
+
+export { Database };
